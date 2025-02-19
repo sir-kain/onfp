113e4bfb13d7a6aa2564e80dcdcbaf75a9415693
Mouhamadou Makhtar Ly <mouhamadoumakhtarly@gmail.com>
2025-02-19 20:23:43 +0000

    Ajout du nouveau bouton

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,9 @@ class User extends Authenticatable implements HasName
         'name',
         'email',
         'password',
+        'prenom',
+        'telephone'
+        'adress',
     ];
 
     /**
